2f864dfb95c839ab428a2df964b3539991df96db
fffonion <fffonion@gmail.com>
2013-02-13 16:42:30 +0800

    1.35
    修复bug

--- RoameBot.py
+++ RoameBot.py
@@ -3,63 +3,75 @@
 # Contributor:
 #      fffonion		<fffonion@gmail.com>
 
-__version__ = '1.34'
+__version__ = '1.35'
 
 import urllib2,re,os,time,ConfigParser,sys,traceback,socket
 PICLIST=[]
 #INITURL='http://www.roame.net/index/hakuouki-shinsengumi-kitan'
 HOMEURL='http://www.roame.net'
 RATIO_SUFFIX=['','-wall','-16x10','-16x9','-4x3','-5x4','-oall','-wgth','-wlth','-weqh']
-BUILT_IN_SUFFIX=['','-pic-16x9','-pic-16x10','-pic-4:3','-5:4','-wgth','-pic-wlth','-pic-wegh','','',\
+BUILT_IN_SUFFIX=['','-pic-16x9','-pic-16x10','-pic-4:3','-pic-5:4','-pic-wgth','-pic-wlth','-pic-weqh','','',\
 				'-hotest-down','-hotest-weeklydown','-hotest-monthlydown','-hotest-score',\
 				'-hotest-monthlyscore','-others-latest','-others-random']
 GET_INTERVAL=0.2
 LOGPATH='roamebot.log'
-#urllib2 HOOk was based on:http://stackoverflow.com/questions/2028517/python-urllib2-progress-hook
+#urllib2 HOOk 基于 http://stackoverflow.com/questions/2028517/python-urllib2-progress-hook
 def chunk_report(bytes_got, chunk_size, total_size,init_time):
+	'''
+	A hook for progress callback
+	'''
 	percent = float(bytes_got) / total_size
 	percent = round(percent*100, 2)
-	eta=time.strftime('%M:%S', time.localtime((time.time()-init_time)*(100-percent)/percent))
-	proglength=30
-	progressbar='#'*(proglength*int(percent)/100)+' '*(proglength-proglength*int(percent)/100)
-	backspace='\b'*140
+	eta=time.strftime('%M:%S', time.localtime((time.time()-init_time)*(100-percent)/percent))#剩余时间
+	proglength=30#进度条长度
+	progressbar='#'*(proglength*int(percent)/100)+' '*(proglength-proglength*int(percent)/100)#计算进度条
+	backspace='\b'*140#同一行打印的退格
 	print "%4.1f%% [%s]     %5d/%5dKB   %s ETA%s" % (percent,progressbar,bytes_got/1024,total_size/1024, eta,backspace),
-	if bytes_got >= total_size:
+	if bytes_got >= total_size:#完成时
 		print backspace,
 
 def urlget(src,getimage=False,retries=3,chunk_size=8,downloaded=-1):
+	'''
+	urllib2 download module
+	'''
 	try:
+		#构造request
 		req = urllib2.Request(src)
 		req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.94 Safari/537.4')
 		#req.add_header('Referer', 'http://www.google.com/xxxx')
+		#打开回复
 		resp = urllib2.urlopen(req)
 		resp.info()
 		content=''
 		total_size=None
-		if getimage:
+		#处理内容
+		if getimage:#显示进度的话
 			while total_size==None:
 				total_size=resp.info().getheader('Content-Length')
-				time.sleep(GET_INTERVAL)#F*CK!!!!!!!
-			total_size = int(total_size.strip())#html has no content-length
+				time.sleep(GET_INTERVAL)#F*CK!!!!!!!服务器太捉鸡……
+			total_size = int(total_size.strip())
+			#用头直接判断是否已下载
 			if total_size==downloaded:
 				return 'SAME'
+			#初始化变量
 			chunk_size*=1024
 			bytes_got = 0
 			init_time=time.time()
+			#开始chunk read
 			while 1:
 				chunk = resp.read(chunk_size)
 				content+=chunk
 				bytes_got += len(chunk)
-				if not chunk:
+				if not chunk:#完成
 					break
-				if getimage:
-					chunk_report(bytes_got, chunk_size, total_size,init_time)
+				chunk_report(bytes_got, chunk_size, total_size,init_time)
 			#content=chunk_read(resp, chunk*1024,chunk_report)
-		else:
+		else:#直接读取
 			content = resp.read()#.decode('utf-8')
 		return content
-	except urllib2.URLError, e:  
+	except urllib2.URLError, e:#错误处理 
  		if isinstance(e.reason, socket.timeout):  
+			#重试处理
 			if retries>0:
 				return urlget(src,getimage,retries-1)
 			else:
@@ -86,8 +98,13 @@ def urlget(src,getimage=False,retries=3,chunk_size=8,downloaded=-1):
 #	for i in range(len(lst)):
 #		lstnew[i]=lst[i].decode('utf-8')
 #	return lstnew
+
 def print_c(str):
+	'''
+	UTF-8 print module
+	'''
 	print str.decode('utf-8')
+	
 def parse_albumname(url):
 	'''
 	Return albumname TUPLE: 0=CHN, 1=ENG, 2=JPN
@@ -101,12 +118,14 @@ def parse_albumname(url):
 	return albumname[0]
 
 def parse_entry(url):
-	#print url
+	'''
+	Return entry list
+	'''
 	content=urlget(url)
 	entries=[]
 	#exp <strong style="margin:0px;padding:0px">中二病也要谈恋爱 BD VOL.1</strong>
 	allentries=re.findall('<h2>(.*?)</strong>',content,re.DOTALL)
-	for i in range(len(allentries)):#link num name
+	for i in range(len(allentries)):
 		entries.append([re.findall('href="(.+)">',allentries[i])[0],\
 					re.findall('2px">(\d+)</span',allentries[i])[0],
 					re.findall('0px">(.+)$',allentries[i])[0]])
@@ -114,34 +133,42 @@ def parse_entry(url):
 	return entries
 
 def parse_pagelist(url,pagenum,mode=0):
+	'''
+	Return pic list, elem is dict
+	'''
 	global PICLIST
 	#pic_structure={'index':0,'thumb':'','full':'','width':0,'height':0,'length':0}
 	content=urlget(url)
-	singlepic=re.findall('/h[23]>(.*?)<div',content,re.DOTALL)#singlepic contains thumb and full-size page url
+	singlepic=re.findall('/h[23]>(.*?)<div',content,re.DOTALL)#singlepic 包含 thumb and full-size page url
 	picinfo=re.findall('font-size:12px;font-weight:bold.*\">(.+) - ([0-9.]+)([A-Z]+)</div',content)
-	if picinfo==[]:
-		picinfo=re.findall('r:#789">([0-9.]+)([A-Z]+)</span',content)#today mode
+	if picinfo==[]:#有两种情况，以下对today模式
+		picinfo=re.findall('r:#789">([0-9.]+)([A-Z]+)</span',content)
 	picsize=re.findall('<strong>(\d+)×(\d+)</strong>',content)
-	for i in range(len(singlepic)):	#len(singlepic)#pic num added
+	#具体处理
+	for i in range(len(singlepic)):	
 		PICLIST.append({'index':0,'thumb':'','full':'','height':0,'width':0,'length':0,'format':''})
 		PICLIST[-1]['index']=i+(pagenum-1)*8
-		fullsizepageurl=re.findall('href=\"(.+)\"><img',singlepic[i])[0]#full-size
+		fullsizepageurl=re.findall('href=\"(.+)\"><img',singlepic[i])[0]#原图url
 		PICLIST[-1]['full']=parse_fullsize(HOMEURL+fullsizepageurl.replace(HOMEURL,''))
-		PICLIST[-1]['thumb']=re.findall('src=\"(.+)\/>',singlepic[i])[0]#thumbnail
-		PICLIST[-1]['width']=picsize[i][0]
-		PICLIST[-1]['height']=picsize[i][1]
+		PICLIST[-1]['thumb']=re.findall('src=\"(.+)\/>',singlepic[i])[0]#缩略图url
+		PICLIST[-1]['width']=picsize[i][0]#图宽
+		PICLIST[-1]['height']=picsize[i][1]#图高
+		#图片文件长度
 		if(picinfo[i][-1]=='MB'):
-			PICLIST[-1]['length']=float(picinfo[i][-2])*1024#wrap with float()
+			PICLIST[-1]['length']=float(picinfo[i][-2])*1024#float化防止变int
 		else:
 			PICLIST[-1]['length']=picinfo[i][-2]
-		PICLIST[-1]['format']=len(picinfo[i])==2 and 'UNKNOWN' or picinfo[i][0]#today mode no format
-	nextpage=re.findall('title="下一页" href="(.+)" style=',content)
-	if (nextpage==[]):
+		PICLIST[-1]['format']=len(picinfo[i])==2 and 'UNKNOWN' or picinfo[i][0]#today模式没有文件格式
+	nextpage=re.findall('title="下一页" href="(.+)" style=',content)#下一页
+	if (nextpage==[]):#最后一页
 		return None
 	else:
 		return HOMEURL+nextpage[0]
 	
 def parse_fullsize(url):
+	'''
+	Return image url
+	'''
 	content=urlget(url)
 	deeperpage=re.findall('darlnks">.+index.html.+href="(.*?)" style.+display:block',content,re.DOTALL)#goto download page
 	content=urlget(deeperpage[0])
@@ -149,6 +176,9 @@ def parse_fullsize(url):
 	return picurl[0]
 
 def fmttime():
+	'''
+	Return time like [2013-02-13 16:23:21]
+	'''
 	return '['+time.strftime('%Y-%m-%d %X',time.localtime())+'] '
 #def down_callback(saved,blocksize,total):
 #	x = 100.0 * saved * blocksize / total  
@@ -157,6 +187,9 @@ def fmttime():
 #	print '%.2f%%' % x
 
 def read_config(sec,key):
+	'''
+	Read config from file
+	'''
 	cf=ConfigParser.ConfigParser()
 	cf.read(os.getcwdu()+os.path.sep+'config.ini')
 	val=cf.get(sec, key)
@@ -166,12 +199,18 @@ def read_config(sec,key):
 		return val
 
 def write_config(sec,key,val):
+	'''
+	Write config to file
+	'''
 	cf=ConfigParser.ConfigParser()
 	cf.read(os.getcwdu()+os.path.sep+'config.ini')
 	cf.set(sec, key,val)
 	cf.write(open(os.getcwdu()+os.path.sep+'config.ini', "w"))
 
 def init_config():
+	'''
+	First run, initialize config.ini
+	'''
 	filename = os.getcwdu()+os.path.sep+'config.ini'
 	f=file(filename,'w')
 	f.write('[download]\nskip_exist = 2\ndownload_when_parse = 1\ntimeout = 10\nchunksize = 8\nretries = 3\ndir_name = 2\ndir_path = \nname = hyouka\nbuilt_in = 21\nfilter = filter_0\nfirst_page_num = \nproxy = \nproxy_name = \nproxy_pswd = \n[filter_0]\nratio = 1\nmax_length = \nmax_width = \nmin_length = \nmin_width = \nmax_size = \nmin_size = ')
@@ -179,6 +218,9 @@ def init_config():
 	f.close() 
 
 def init_proxy():
+	'''
+	Install proxy opener
+	'''
 	if read_config('download','proxy')!='':
 		proxy_support = urllib2.ProxyHandler({'http':'http://['+read_config('download','proxy_name')\
 											+']:['+read_config('download','proxy_pswd')+']@['\
@@ -187,9 +229,7 @@ def init_proxy():
 		urllib2.install_opener(opener)
 #main entry
 def main():
-	global PICLIST
-	PICLIST=[]
-	#read and set config vals
+	#读取并设置变量
 	timeout=read_config('download','timeout')
 	socket.setdefaulttimeout(int(timeout))
 	chunk=int(read_config('download','chunksize'))
@@ -199,18 +239,22 @@ def main():
 	dir_path=read_config('download','dir_path')=='' and os.path.abspath(os.curdir)	or read_config('download','dir_path')
 	#LOGPATH=read_config('download','logpath')
 	firstpagenum=read_config('download','first_page_num')
-	if firstpagenum=='':
+	if firstpagenum=='':#未指定默认为无限制
 		firstpagenum=2147483647
 	else:
 		firstpagenum=int(firstpagenum)
 	projname=read_config('download','name')
-	#determine first page
-	if projname=='' or 0<projname<20:
+	global PICLIST
+	PICLIST=[]
+	nextpage=[]
+	###################开始预处理
+	#决定首页
+	if projname=='' or 0<projname<20:#today模式
 		print_c('没有指定名称,按照快速筛选(built_in)选项下载')
-		nextpage=HOMEURL+'/today/index'+BUILT_IN_SUFFIX[int(read_config('download','built_in'))]+'.html'
+		nextpage.append(HOMEURL+'/today/index'+BUILT_IN_SUFFIX[int(read_config('download','built_in'))]+'.html')
 		namelist=[time.strftime('%Y-%m-%d %H-%M-%S',time.localtime()),'','']
 		DIR_NAME=0#only this one
-	else:
+	else:#正常模式OR散图模式
 		if projname=='misc':#散图模式
 			print_c('输入散图的年月，如201301，最早为200604:')
 			yyyymm=raw_input()
@@ -227,15 +271,17 @@ def main():
 				entry=entry[0][0]
 		print_c(fmttime()+'Collecting info for : '+namelist[0]+'/'+namelist[1]+'/'+namelist[2])
 		filtername=read_config('download','filter')
+		#处理比例过滤器
 		ratiolist=read_config(filtername,'ratio').split('|')
-		nextpage=[]
-		for r in range(len(ratiolist)):
+		for r in range(len(ratiolist)):#依次构造
 			nextpage.append(HOMEURL+entry+'/index'+RATIO_SUFFIX[int(ratiolist[r].strip())]+'.html')
 	WORKINGDIR=(dir_path+'/'+((namelist[2]=='' and DIR_NAME=='2')and namelist[0] or namelist[int(DIR_NAME)]))\
-	.decode('utf-8')
+	.decode('utf-8')#保存目录
 	if not os.path.exists(WORKINGDIR):
 		os.mkdir(WORKINGDIR)
 	pagenum=1
+	###################开始主处理
+	#页面处理并得到所有图片URL，位于全局变量PICLIST中
 	for j in range(len(nextpage)):
 		while(nextpage[j]!=None and pagenum<=firstpagenum):
 			print '%sPage parsing started at %s' % (fmttime(),nextpage[j])
@@ -244,11 +290,12 @@ def main():
 			#print PICLIST
 	print fmttime()+'Parse finished.'
 	print fmttime()+'Download started.'
+	#图片下载
 	for i in range(len(PICLIST)):
 		#time.sleep(GET_INTERVAL)#f*ck!!!
-		basename=re.findall('/([A-Za-z0-9._]+)$',PICLIST[i]['full'])[0]
+		basename=re.findall('/([A-Za-z0-9._]+)$',PICLIST[i]['full'])[0]#切割文件名
 		filename=WORKINGDIR+'/'+basename
-		#urllib.urlretrieve(PICLIST[i]['full'], filename,down_callback)  
+		#urllib.urlretrieve(PICLIST[i]['full'], filename,down_callback)
 		if os.path.exists(filename) and SKIP_EXIST=='1':#存在则跳过
 				print '\bSkip '+basename+': Exists.'+' '*35
 		elif os.path.exists(filename) and SKIP_EXIST=='2' and \
@@ -257,23 +304,29 @@ def main():
 		else:#不存在 或 2&&大小不符
 			print '\b%sDownloading %3d/%3d images: %s ->' % (fmttime(),i+1,len(PICLIST),basename)
 			#       |不知道为什么会空一格…所以加上退格…
+			#保存到文件
 			fileHandle=open(filename,'wb')
 			fileHandle.write(urlget(PICLIST[i]['full'],True,retries,chunk))
 			fileHandle.close()
 	print '\n'+fmttime()+'Download finished.\n'+str(len(PICLIST))+' pictures saved under \"'+WORKINGDIR
 	
 def search():
+	'''
+	Search module
+	'''
 	reslist=[]
+	#从索引页处理所有名称
 	content=urlget('http://www.roame.net/index')
 	#exp:<div class="l2"><a href="/index/kikis-delivery-service">魔女宅急便 - Kiki's Delivery Service</a></div>
 	list=re.findall('<a href="/index/([a-z-]+)">(.+) - (.+)</a>',content)
+	#询问输入
 	input=raw_input('Input your keyword:')
 	if sys.platform=='win32':
 		input=input.decode('gb2312')
 	else:
 		input=input.decode('utf-8')
 	count=0
-	#print list
+	#顺序查找并打印
 	for i in range(len(list)):
 		if re.search(input.encode('utf-8'), list[i][1], re.IGNORECASE) or \
 		re.search(input, list[i][2], re.IGNORECASE):
@@ -293,8 +346,11 @@ def search():
 			main()
 		else:
 			print_c('别乱按，熊孩子o(￣ヘ￣o＃) \n')
-	#print reslist
+	
 def quick_filter():
+	'''
+	Today mode/Quick filter/Unsorted
+	'''
 	print_c('显示快速过滤选项：')
 	print_c('0.所有最新\t10.今日下载排行\t15.大家刚下载的\n1.最新16:9\t11.一周下载排行\t16.随机八张图片\n2.最新16:10\t12.30天下载排行\t17.未分类画集\n3.最新4:3\t13.一周评分排行\t18.未分类散图\n4.最新5:4\t14.30天评分排行\n5.最新其他横向\n6.最新竖向\n7.最新等宽\t')
 	try:
@@ -309,10 +365,13 @@ def quick_filter():
 		else:
 			raise Exception('', '')
 		main()
-	except ValueError:
+	except ValueError:#熊孩子没有输入数字
 		print_c('[なに？]σ(· ·?) \n')
 
 def update():
+	'''
+	Online update
+	'''
 	newver=urlget("https://raw.github.com/fffonion/RoameBot/master/version.txt")
 	if newver!=__version__:
 		print_c('花现新版本：'+newver)
@@ -329,13 +388,16 @@ def update():
 		print_c('\n更新到了版本：'+newver)
 	else:
 		print_c('已经是最新版本啦更新控：'+__version__)
+		
 if __name__ == '__main__':  
 	try:
 		if not os.path.exists(os.getcwdu()+os.path.sep+'config.ini'):#first time
 			init_config()
 		init_proxy()
+		#重设默认编码
 		reload(sys)
 		sys.setdefaultencoding('utf-8')
+		#菜单
 		print_c('「ロアメボット。」'+__version__+'  ·ω·）ノ')
 		while input !='5':
 			print_c('1.搜索\n2.快速筛选\n3.继续上次任务\n4.更新\n5.退出\n')
@@ -354,8 +416,8 @@ if __name__ == '__main__':
 		print_c('啊咧，出错了( ⊙ o ⊙ )~ ('+str(ex)+')\n错误已经记载在roamebot.log中')
 		f=open(os.getcwdu()+os.path.sep+'romaebot.log','a')
 		f.write(fmttime()+'Stopped.\n')
-		#traceback.print_exc(file=f)
-		traceback.print_exc()
+		traceback.print_exc(file=f)
+		#traceback.print_exc()
 		f.flush()
 		f.close()
 print_c("按任意键退出亲~")
