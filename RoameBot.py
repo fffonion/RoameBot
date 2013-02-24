53d68dc426482b5530d373d79792996f87659923
fffonion <fffonion@gmail.com>
2013-02-24 20:43:38 +0800

    2.0beta
    多线程下载

--- RoameBot.py
+++ RoameBot.py
@@ -3,10 +3,11 @@
 # Contributor:
 #      fffonion		<fffonion@gmail.com>
 
-__version__ = '1.63'
+__version__ = '2.0beta'
 
-import urllib2,re,os,os.path as opath,time,ConfigParser,sys,traceback,socket
-PICLIST=[]
+import urllib2,re,os,os.path as opath,time,ConfigParser,sys,traceback,socket,threading,Queue,random
+PICQUEUE=Queue.Queue()
+REPORTQUEUE=Queue.Queue()
 FILTER={}
 INDEXLIST=[]
 #INITURL='http://www.roame.net/index/hakuouki-shinsengumi-kitan'
@@ -17,33 +18,32 @@ RATIO_SUFFIX=['','-wall','-16x10','-16x9','-4x3','-5x4','-oall','-wgth','-wlth',
 BUILT_IN_SUFFIX=['','-pic-16x9','-pic-16x10','-pic-4x3','-pic-5x4','-pic-wgth','-pic-wlth','-pic-weqh','','',\
 				'-hotest-down','-hotest-weeklydown','-hotest-monthlydown','-hotest-score',\
 				'-hotest-monthlyscore','-others-latest','-others-random']
+THREAD_NAME=['Almond','Banana','Cherry','Damson','Emblic']
 GET_INTERVAL=0.1
-LOGPATH='roamebot.log'
-#urllib2 HOOk 基于 http://stackoverflow.com/questions/2028517/python-urllib2-progress-hook
-def chunk_report(bytes_got, chunk_size, total_size,init_time):
-	'''
-	A hook for progress callback
-	'''
-	percent = float(bytes_got) / total_size
-	percent = round(percent*100, 2)
-	eta=time.strftime('%M:%S', time.localtime((time.time()-init_time)*(100-percent)/percent))#剩余时间
-	proglength=30#进度条长度
-	progressbar='#'*(proglength*int(percent)/100)+' '*(proglength-proglength*int(percent)/100)#计算进度条
-	backspace='\b'*140#同一行打印的退格
-	print "%4.1f%% [%s]     %5d/%5dKB   %s ETA%s" % (percent,progressbar,bytes_got/1024,total_size/1024, eta,backspace),
-	if bytes_got >= total_size:#完成时
-		print backspace,
+THREADS=5
 
-def urlget(src,getimage=False,retries=3,chunk_size=8,downloaded=-1,referer=''):
+THREAD_PROGRESS=[[0,0,0,0,0]]*THREADS#已下载，总大小，开始时间，总下载量，总下载大小
+LOGPATH='roamebot.log'
+COOKIE=['uid=149448; upw=a3119b10b8cb23e19053e99b82bb4ea4; cmd=CY%408Tl8Z9GjXgp6p2UhqvJTHo3D7sVFpC;',\
+	'uid=149449; upw=a3119b10b8cb23e19053e99b82bb4ea4; cmd=JP2SkSeeeb3hsrk7CST3XxmhpuY4Gszt9;',\
+	'uid=149458; upw=a3119b10b8cb23e19053e99b82bb4ea4; cmd=JxKBgjlypSuBk4zkhJ3BtNrjdDUDq6yTb;',\
+	'uid=149459; upw=a3119b10b8cb23e19053e99b82bb4ea4; cmd=JuYHdgPPyJQsh59JjaIUWFfkiWPmvBepi;',\
+	'uid=149460; upw=a3119b10b8cb23e19053e99b82bb4ea4; cmd=JwGe6d%4022UwKg2sYCjUsvzdrZZIp9oFDH;']
+	
+def urlget(src,getimage=False,retries=3,chunk_size=8,downloaded=-1,referer='',cookieid=-1):
 	'''
 	urllib2 download module
 	'''
 	#print src
+	global REPORTQUEUE
+	prompt=THREAD_NAME[cookieid]+': '
 	try:
 		#构造request
 		req = urllib2.Request(src)
 		req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.94 Safari/537.4')
 		#req.add_header('Referer', referer)
+		if cookieid!=-1:
+			req.add_header('Cookie', COOKIE[cookieid])
 		#打开回复
 		resp = urllib2.urlopen(req)
 		#resp.info()
@@ -61,28 +61,35 @@ def urlget(src,getimage=False,retries=3,chunk_size=8,downloaded=-1,referer=''):
 					if sleep_retry>27:#最多睡三次
 						total_size='-1'#链接错误flag
 					else:
-						print(fmttime()+'Got plain content. Retrying in '+str(GET_INTERVAL*sleep_retry)+'s.')
+						REPORTQUEUE.put(fmttime()+prompt+'Got plain content. Retrying in '+str(GET_INTERVAL*sleep_retry)+'s.')
 						sleep_retry*=3
 			total_size = int(total_size.strip())
 			if total_size<=8843:#链接错误=-1或过期=8843
-				print fmttime()+'Url expired or broken. Reparsing from referer page.'
+				REPORTQUEUE.put(fmttime()+prompt+'Url expired or broken. Reparsing from referer page.')
 				content=urlget(parse_fullsize(referer),getimage,retries,chunk_size,downloaded,referer)
 			else:#正常下载
 				#用头信息直接判断是否已下载
-				if total_size==downloaded:
-					return 'SAME'
+				if downloaded!=-1:
+					if total_size==downloaded:
+						return 'SAME'
+					else:
+						return 'NOT-SAME'
 				#初始化变量
-				chunk_size*=1024
 				bytes_got = 0
-				init_time=time.time()
+				#init_time=time.time()
 				#开始chunk read
 				while 1:
-					chunk = resp.read(chunk_size)
+					chunkrand=chunk_size*random.randint(800,1248)
+					chunk = resp.read(chunkrand)
 					content+=chunk
 					bytes_got += len(chunk)
 					if not chunk:#完成
 						break
-					chunk_report(bytes_got, chunk_size, total_size,init_time)
+					global THREAD_PROGRESS
+					THREAD_PROGRESS[cookieid-1][0]=bytes_got
+					THREAD_PROGRESS[cookieid-1][1]=total_size
+					THREAD_PROGRESS[cookieid-1][4]+=chunkrand
+					#chunk_report(bytes_got, chunk_size, total_size,init_time)
 				#content=chunk_read(resp, chunk*1024,chunk_report)
 		else:#直接读取
 			content = resp.read()#.decode('utf-8')
@@ -92,7 +99,7 @@ def urlget(src,getimage=False,retries=3,chunk_size=8,downloaded=-1,referer=''):
  		print e.code
  		if retries>0:#重试处理
 	 		#if isinstance(e.reason, socket.timeout):
-	 		print fmttime()+'Error['+str(e.code)+']',
+	 		print fmttime()+prompt+'Error['+str(e.code)+']',
 	 		if e.code==10060:#超时
 				print 'Connection timed out. Retrying '+str(retries)+' times.'
 			elif  e.code>=400:#客户端错误或服务器错误
@@ -103,26 +110,7 @@ def urlget(src,getimage=False,retries=3,chunk_size=8,downloaded=-1,referer=''):
 				print 'Failed on '+src
 				return None
 		
-		
-#NOT USING CURRENTLY	
-#class multiget(threading.Thread):
-#	def __init__(self,url):
-#		threading.Thread.__init__(self)
-#		self.thread_stop = False
-#		self.geturl=url
-#
-#	def run(self):
-#		print len(urlget(self.geturl))
-#		self.stop()
-#	def stop(self):
-#		self.thread_stop = True
-  
-#def decutf(lst):
-#	lstnew=[i for i in range(len(lst))]
-#	for i in range(len(lst)):
-#		lstnew[i]=lst[i].decode('utf-8')
-#	return lstnew
-
+       
 def print_c(str):
 	'''
 	UTF-8 print module
@@ -134,7 +122,88 @@ def normstr(str,errors='ignore'):
 		return str.encode('gbk',errors)
 	else:
 		return str.encode('utf-8',errors)
-	
+def fmttime():
+	'''
+	Return time like [2013-02-13 16:23:21]
+	'''
+	return '['+time.strftime('%Y-%m-%d %X',time.localtime())+'] '
+
+class getimgthread(threading.Thread):
+	def __init__(self,threadname,workingdir,skip_exist,retries=3,chunk_size=8,downloaded=-1):
+		threading.Thread.__init__(self, name=threadname)
+		self.id=int(self.getName())
+		self.workingdir=workingdir
+		self.skip_exist=skip_exist
+		self.retries=retries
+		self.chunk_size=chunk_size
+		self.downloaded=downloaded
+		self.propmt=THREAD_NAME[self.id-1]+': '
+		global THREAD_PROGRESS
+		THREAD_PROGRESS[self.id-1]=[0]*5
+	def run(self):
+		global REPORTQUEUE,THREAD_PROGRESS
+		REPORTQUEUE.put(fmttime()+self.propmt+'Started.')
+		while not PICQUEUE.empty():
+			self.src=PICQUEUE.get()
+			basename=re.findall('/([A-Za-z0-9._]+)$',self.src['full'])[0]#切割文件名
+			filename=self.workingdir+opath.sep+basename
+			#urlget(src,getimage=False,retries=3,chunk_size=8,downloaded=-1,referer='',cookieid=-1):
+			if opath.exists(filename) and self.skip_exist=='1':#存在则跳过
+					REPORTQUEUE.put(fmttime()+self.propmt+'Skip '+basename+': Exists.'+' '*10)
+					THREAD_PROGRESS[self.id-1][3]+=1
+			elif opath.exists(filename) and self.skip_exist=='2' and \
+			urlget(self.src['full'],True,self.retries,self.chunk_size,opath.getsize(filename),\
+				self.src['referpage'],self.id-1)=='SAME':
+					REPORTQUEUE.put(fmttime()+self.propmt+'Skip '+basename+': Same size.'+' '*5)
+					THREAD_PROGRESS[self.id-1][3]+=1
+			else:#不存在 或 2&&大小不符
+				REPORTQUEUE.put(fmttime()+self.propmt+'Start '+basename+'.')
+				#print '\b%sDownloading %3d/%3d images: %s ->' % (fmttime(),i+1,PICQUEUE.qsize(),basename)
+				#       |不知道为什么会空一格…所以加上退格…
+				#设置监视起始点
+				THREAD_PROGRESS[self.id-1][2]=time.time()
+				#保存到文件
+				fileHandle=open(filename,'wb')
+				fileHandle.write(urlget(self.src['full'],True,self.retries,self.chunk_size,-1,\
+									self.src['referpage'],self.id-1))
+				fileHandle.close()
+				REPORTQUEUE.put(fmttime()+self.propmt+'Finish '+basename+'.')
+				THREAD_PROGRESS[self.id-1][3]+=1
+		THREAD_PROGRESS[self.id-1][2]=-1#退出标识
+		REPORTQUEUE.put(fmttime()+self.propmt+'Exit~')
+		
+class reportthread(threading.Thread):
+	def __init__(self,threadname='Reportter'):
+		threading.Thread.__init__(self, name=threadname)
+	def run(self):
+		#THREAD_PROGRESS=[[0,0,0,0]]*THREADS#已下载，总大小，开始时间
+		init_time=time.time()
+		backspace='\b'*140
+		flush=' '*65
+		livethread=THREADS
+		global REPORTQUEUE
+		lastdownsize=0
+		sleeptime=0.2
+		while livethread>0:
+			downcount=0
+			queuesize=0
+			downloadsize=0
+			while not REPORTQUEUE.empty():
+				print(flush+backspace+REPORTQUEUE.get())
+			livethread=THREADS
+			for i in range(THREADS):
+				downcount+=THREAD_PROGRESS[i][3]
+				queuesize+=THREAD_PROGRESS[i][1]
+				downloadsize+=THREAD_PROGRESS[i][4]
+				if THREAD_PROGRESS[i][2]==-1:
+					livethread-=1
+			#eta=time.strftime('%M:%S', time.localtime((time.time()-init_time)*(100-percent)/percent))
+			elapse=time.strftime('%M:%S',time.localtime(time.time()-init_time))
+			print "\bThread %d/%d  Remain %3d/%3d  Queued %3d/%3d   %3.1fKB/s    %s%s" % (livethread,THREADS,\
+				PICQUEUE.qsize()-downcount,downcount+PICQUEUE.qsize()+5,downloadsize/1024,queuesize/1024,\
+				(downloadsize-lastdownsize)/sleeptime/1024,elapse,backspace),
+			lastdownsize=downloadsize
+			time.sleep(sleeptime)
 def parse_albumname(url):
 	'''
 	Return albumname TUPLE: 0=CHN, 1=ENG, 2=JPN
@@ -169,7 +238,7 @@ def parse_pagelist(url,pagenum,mode=0):
 	'''
 	Return pic list, elem is dict
 	'''
-	global PICLIST
+	global PICQUEUE
 	up_to_date=False
 	today_mode=False
 	#pic_structure={'index':0,'thumb':'','full':'','width':0,'height':0,'length':0}
@@ -199,16 +268,17 @@ def parse_pagelist(url,pagenum,mode=0):
 		if FILTER['min_width']<=float(picsize[i][0])<=FILTER['max_width'] and \
 		FILTER['min_length']<=float(picsize[i][1])<=FILTER['max_length'] and \
 		FILTER['min_size']<=float(piclength)<=FILTER['max_size'] and (not picupload[i] in FILTER['banned_uploader']):
-			PICLIST.append({'index':0,'thumb':'','referpage':'','full':'','height':0,'width':0,'length':0,'format':''})
-			PICLIST[-1]['index']=i+(pagenum-1)*8
+			picelem={'index':0,'thumb':'','referpage':'','full':'','height':0,'width':0,'length':0,'format':''}
+			picelem['index']=i+(pagenum-1)*8
 			fullsizepageurl=re.findall('href=\"(.+)\"><img',singlepic[i])[0]#原图url
-			PICLIST[-1]['referpage']=HOMEURL+fullsizepageurl.replace(HOMEURL,'')
-			PICLIST[-1]['full']=parse_fullsize(PICLIST[-1]['referpage'])
-			PICLIST[-1]['thumb']=re.findall('src=\"(.+)\"\/>',singlepic[i])[0]#缩略图url
-			PICLIST[-1]['width']=picsize[i][0]#图宽
-			PICLIST[-1]['height']=picsize[i][1]#图高
-			PICLIST[-1]['length']=piclength
-			PICLIST[-1]['format']=len(picinfo[i])==2 and 'UNKNOWN' or picinfo[i][0]#today模式没有文件格式
+			picelem['referpage']=HOMEURL+fullsizepageurl.replace(HOMEURL,'')
+			picelem['full']=parse_fullsize(picelem['referpage'])
+			picelem['thumb']=re.findall('src=\"(.+)\"\/>',singlepic[i])[0]#缩略图url
+			picelem['width']=picsize[i][0]#图宽
+			picelem['height']=picsize[i][1]#图高
+			picelem['length']=piclength
+			picelem['format']=len(picinfo[i])==2 and 'UNKNOWN' or picinfo[i][0]#today模式没有文件格式
+			PICQUEUE.put(picelem)
 	nextpage=re.findall('title="下一页" href="(.+)" style=',content)#下一页
 	if nextpage==[] or up_to_date:#最后一页或已把更加新的处理完
 		return None
@@ -225,11 +295,6 @@ def parse_fullsize(url):
 	picurl=re.findall('src="(.+)" style="background',content)
 	return picurl[0]
 
-def fmttime():
-	'''
-	Return time like [2013-02-13 16:23:21]
-	'''
-	return '['+time.strftime('%Y-%m-%d %X',time.localtime())+'] '
 #def down_callback(saved,blocksize,total):
 #	x = 100.0 * saved * blocksize / total  
 #	if x > 100:  
@@ -343,41 +408,45 @@ def init_proxy():
 		opener = urllib2.build_opener(proxy_support, urllib2.HTTPHandler)
 		urllib2.install_opener(opener)
 		
-def load_remote_piclist(nextpage,firstpagenum,workingdir,ratiolist):
+def load_remote_picqueue(nextpage,firstpagenum,workingdir,ratiolist):
 	'''
-	Remote PICLIST parsing
+	Remote PICQUEUE parsing
 	'''
-	#global PICLIST
-	#页面处理并得到所有图片URL，位于全局变量PICLIST中
+	#global PICQUEUE
+	#页面处理并得到所有图片URL，位于全局变量PICQUEUE中
 	pagenum=1
-	for j in range(len(nextpage)):
+	for j in range(len(nextpage)):#nextpage列表个数等于比例个数
 		if read_timestamp(workingdir,ratiolist[j])==True:
 			print fmttime()+'Read time-stamp info from file.'
 		while(nextpage[j]!=None and pagenum<=firstpagenum):
 			print '%sPage parsing started at %s' % (fmttime(),nextpage[j])
 			nextpage[j]=parse_pagelist(nextpage[j],pagenum)
 			pagenum+=1
-			#print PICLIST
 	#保存到文件
 	file=open(workingdir+opath.sep+'.roameproject','w')
-	for i in range(len(PICLIST)):
-		file.write(str(PICLIST[i]['index'])+','+PICLIST[i]['thumb']+','+PICLIST[i]['referpage']+','+PICLIST[i]['full']+','\
-				+str(PICLIST[i]['height'])+','+str(PICLIST[i]['width'])+','+str(PICLIST[i]['length'])+','\
-				+PICLIST[i]['format']+'\n')
+	queindex=1
+	while queindex <= PICQUEUE.qsize():
+		#print queindex,PICQUEUE.qsize()
+		picelem=PICQUEUE.get()
+		PICQUEUE.put(picelem)
+		file.write(str(picelem['index'])+','+picelem['thumb']+','+picelem['referpage']+','+picelem['full']+','\
+				+str(picelem['height'])+','+str(picelem['width'])+','+str(picelem['length'])+','\
+				+picelem['format']+'\n')
+		queindex+=1
 	file.close()
 	
-def load_local_piclist(projfile_path):
+def load_local_picqueue(projfile_path):
 	'''
-	read PICLIST from .roameproject
+	read PICQUEUE from .roameproject
 	'''
-	global PICLIST
+	global PICQUEUE
 	file=open(projfile_path,'r')
 	for line in file:
 		list=(line[-1]=='\n' and line[:-1] or line).split(',')
-		PICLIST.append({'index':int(list[0]),'thumb':list[1],'referpage':list[2],'full':list[3],'height':int(list[4]),\
+		PICQUEUE.put({'index':int(list[0]),'thumb':list[1],'referpage':list[2],'full':list[3],'height':int(list[4]),\
 					'width':int(list[5]),'length':float(list[6]),'format':list[7]})
 	file.close()
-	return len(PICLIST)
+	return PICQUEUE.qsize()
 	
 #main entry
 def main():
@@ -401,8 +470,8 @@ def main():
 	filtername=read_config('download','filter')
 	load_filter(filtername)
 	ratiolist=read_config(filtername,'ratio').split('|')
-	global PICLIST,FILTER,LASTUPDATE
-	PICLIST=[]
+	global PICQUEUE,FILTER,LASTUPDATE
+	PICQUEUE=Queue.Queue()
 	nextpage=[]
 	
 	###################开始预处理
@@ -447,31 +516,28 @@ def main():
 	###################开始主处理
 	projfile_path=(working_dir+opath.sep+'.roameproject').decode('utf-8')
 	if opath.exists(projfile_path) and opath.getsize(projfile_path)!=0:
-		load_local_piclist(projfile_path)
-		print fmttime()+'Load download progress from file. (Got '+str(len(PICLIST))+'p)'
+		load_local_picqueue(projfile_path)
+		print fmttime()+'Load download progress from file. (Got '+str(PICQUEUE.qsize())+'p)'
 	else:
-		load_remote_piclist(nextpage,firstpagenum,working_dir,ratiolist)
-		print fmttime()+'Parse finished. (Got '+str(len(PICLIST))+'p)'
+		load_remote_picqueue(nextpage,firstpagenum,working_dir,ratiolist)
+		print fmttime()+'Parse finished. (Got '+str(PICQUEUE.qsize())+'p)'
 	print fmttime()+'Download started.'
 	#图片下载
-	for i in range(len(PICLIST)):
-		#time.sleep(GET_INTERVAL)#f*ck!!!
-		basename=re.findall('/([A-Za-z0-9._]+)$',PICLIST[i]['full'])[0]#切割文件名
-		filename=working_dir+opath.sep+basename
-		#urllib.urlretrieve(PICLIST[i]['full'], filename,down_callback)
-		if opath.exists(filename) and skip_exist=='1':#存在则跳过
-				print fmttime()+'Skip '+basename+': Exists.'+' '*10
-		elif opath.exists(filename) and skip_exist=='2' and \
-		urlget(PICLIST[i]['full'],True,retries,chunk,opath.getsize(filename),PICLIST[i]['referpage'])=='SAME':
-				print fmttime()+'Skip '+basename+': Same size exists.'+' '*5
-		else:#不存在 或 2&&大小不符
-			print '\b%sDownloading %3d/%3d images: %s ->' % (fmttime(),i+1,len(PICLIST),basename)
-			#       |不知道为什么会空一格…所以加上退格…
-			#保存到文件
-			fileHandle=open(filename,'wb')
-			fileHandle.write(urlget(PICLIST[i]['full'],True,retries,chunk,-1,PICLIST[i]['referpage']))
-			fileHandle.close()
-	print '\n'+fmttime()+'Download finished.\n'+str(len(PICLIST))+' pictures saved under \"'+working_dir
+	#time.sleep(GET_INTERVAL)#f*ck!!!
+	#(self,threadname,workingdir,skip_exist,retries=3,chunk_size=8,downloaded=-1):
+	threadlist=[getimgthread('1',working_dir,skip_exist,retries,chunk,-1),\
+			getimgthread('2',working_dir,skip_exist,retries,chunk,-1),\
+			getimgthread('3',working_dir,skip_exist,retries,chunk,-1),\
+			getimgthread('4',working_dir,skip_exist,retries,chunk,-1),\
+			getimgthread('5',working_dir,skip_exist,retries,chunk,-1)]
+	for i in range(THREADS):
+		threadlist[i].start()
+	report=reportthread()
+	report.start()
+	for i in range(THREADS):
+		threadlist[i].join()
+	report.join()
+	print '\n'+fmttime()+'Download finished.\n'+str(PICQUEUE.qsize())+' pictures saved under \"'+working_dir
 	os.remove(working_dir+opath.sep+'.roameproject')
 	write_timestamp(working_dir,ratiolist,projname)
 	
