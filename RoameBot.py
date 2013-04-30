43881c27acf71ae5c1a6797852a33685a878c6eb
fffonion <fffonion@gmail.com>
2013-04-30 20:55:09 +0800

    2.3.3.2 增加“随机十个番组”选项（“快速筛选”中） hotfix

--- RoameBot.py
+++ RoameBot.py
@@ -4,7 +4,7 @@
 # Contributor:
 #      fffonion        <fffonion@gmail.com>
 
-__version__ = '2.3.3.1'
+__version__ = '2.3.3.2'
 
 import urllib2,urllib,socket,\
  os,os.path as opath,ConfigParser,sys,traceback,\
@@ -891,6 +891,7 @@ def search_select(input,idx_list):
             _print('别乱按，熊孩子o(￣ヘ￣o＃) \n')
             return False
         else:
+            print 1
             main()
             return False
     return True
@@ -912,6 +913,7 @@ def quick_filter():
             write_config('download','name','misc')
         elif input==17:
             while search_select('RANDOM-TEN-MODE', random.sample(parse_indexlist(),10)):pass
+            return
         else:
             raise ValueError
     except ValueError:#熊孩子没有输入数字
